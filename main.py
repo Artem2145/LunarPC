98a2a8b5d2660d2803617ee247428eb2e6ef48c2
Ph0enixReign <meesenblood@gmail.com>
2024-06-19 15:36:40 +0300

    add message start

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
-print("XYI")
-print("ебал гит")
+import asyncio
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+import time
+
+
+bot = Bot(token="7285822177:AAEro27NLuk8C5thTgJFQ0ofSkPAVugp3qc")
+dp = Dispatcher()
+
+
+
+
+@dp.message(CommandStart())
+async def start_main(message: Message):
+    await message.answer("🌟 Привет! Спасибо, что выбрал именно нашего бота! 🌟\nНаш телеграмм - https://t.me/+LXG_pxKV0AtjMzE6")
+    time.sleep(0.5)
+    await message.answer("Выбери тебе нужный пункт.")
+
+
+
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Exit")
